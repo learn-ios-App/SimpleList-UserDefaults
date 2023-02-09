13224612b60d52b981c84521f950dc7ad2f3005e
Aoitoridayo <s2071150@s.konan-u.ac.jp>
2023-02-10 01:10:42 +0900

    Initial Commit

---
+++ SimpleList/SimpleListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleListApp.swift
+//  SimpleList
+//
+//  Created by 渡邊魁優 on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct SimpleListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
